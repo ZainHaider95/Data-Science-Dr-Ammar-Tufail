c2e413c96bdf122d222da7db7b3e6e8ca3f96549
Zain Haider <96934722+ZainHaider95@users.noreply.github.com>
2022-02-13 14:18:16 +0000

    Add files via upload

---
+++ Python/12_Import_Libraries.py
@@ -0,0 +1,9 @@
+
+# Print value of pi, by importing library
+
+import math
+print("Value of pi is : ", math.pi)
+
+import statistics
+x=[123,45,67,89,98]
+print("Average of array is :", statistics.mean(x))
