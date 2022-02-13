a4e26db876cf95959e47ca8e2e3c8ddfb354f5f4
Zain Haider <96934722+ZainHaider95@users.noreply.github.com>
2022-02-13 14:59:08 +0000

    Add files via upload

---
+++ Python Basics/6_Input_Variables.py
@@ -0,0 +1,23 @@
+'''
+#simple variable assignment
+basket='mangoes'
+print (basket)
+
+
+#Asking from user by Using input function
+basket=input("What's your favourite fruit?  ")
+print(basket)
+
+
+
+#Advance input function: Type 1
+name=input("What's your name? ")
+greetings='Hello '
+print(greetings, name)
+'''
+
+#Advance input function: Type 2
+name=input("What's your name? ")
+age = input ("What's your age? ")
+greetings='Hello '
+print(greetings, name, ", You're still young.")
